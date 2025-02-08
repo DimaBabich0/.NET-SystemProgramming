1f5c3c0042b6b9f327010aabc9e6465669419692
Dima Babich <dimavernigor7@gmail.com>
2025-02-08 22:35:44 +0200

    Uploaded HW02

---
+++ HW02/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace HW02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLaunchProcess = new System.Windows.Forms.Button();
+            this.txtboxProcess = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnKillProcess = new System.Windows.Forms.Button();
+            this.listProcess = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnLaunchProcess
+            // 
+            this.btnLaunchProcess.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLaunchProcess.Location = new System.Drawing.Point(268, 12);
+            this.btnLaunchProcess.Name = "btnLaunchProcess";
+            this.btnLaunchProcess.Size = new System.Drawing.Size(154, 40);
+            this.btnLaunchProcess.TabIndex = 0;
+            this.btnLaunchProcess.Text = "Launch";
+            this.btnLaunchProcess.UseVisualStyleBackColor = true;
+            this.btnLaunchProcess.Click += new System.EventHandler(this.btnLaunchProcess_Click);
+            // 
+            // txtboxProcess
+            // 
+            this.txtboxProcess.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F);
+            this.txtboxProcess.Location = new System.Drawing.Point(12, 15);
+            this.txtboxProcess.Name = "txtboxProcess";
+            this.txtboxProcess.Size = new System.Drawing.Size(250, 35);
+            this.txtboxProcess.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F);
+            this.label1.Location = new System.Drawing.Point(90, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "List of running processes:";
+            // 
+            // btnKillProcess
+            // 
+            this.btnKillProcess.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F);
+            this.btnKillProcess.Location = new System.Drawing.Point(12, 487);
+            this.btnKillProcess.Name = "btnKillProcess";
+            this.btnKillProcess.Size = new System.Drawing.Size(410, 48);
+            this.btnKillProcess.TabIndex = 5;
+            this.btnKillProcess.Text = "Kill process";
+            this.btnKillProcess.UseVisualStyleBackColor = true;
+            this.btnKillProcess.Click += new System.EventHandler(this.btnKillProcess_Click);
+            // 
+            // listProcess
+            // 
+            this.listProcess.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F);
+            this.listProcess.FormattingEnabled = true;
+            this.listProcess.ItemHeight = 28;
+            this.listProcess.Location = new System.Drawing.Point(12, 85);
+            this.listProcess.Name = "listProcess";
+            this.listProcess.Size = new System.Drawing.Size(410, 396);
+            this.listProcess.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 561);
+            this.Controls.Add(this.listProcess);
+            this.Controls.Add(this.btnKillProcess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtboxProcess);
+            this.Controls.Add(this.btnLaunchProcess);
+            this.Name = "Form1";
+            this.Text = "Process manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLaunchProcess;
+        private System.Windows.Forms.TextBox txtboxProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKillProcess;
+        private System.Windows.Forms.ListBox listProcess;
+    }
+}
+
---
+++ HW02/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace HW02
+{
+    public partial class Form1 : Form
+    {
+        private static void ShowInfo(string message)
+        {
+            MessageBox.Show(message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private static void ShowError(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            listProcess.Items.Clear();
+
+            var list = Process.GetProcesses()
+                .OrderBy(x => x.ProcessName)
+                .Select(x => x.ProcessName)
+                .Distinct()
+                .ToArray();
+
+            foreach (var p in list)
+            {
+                listProcess.Items.Add(p);
+            }
+        }
+
+        private void btnLaunchProcess_Click(object sender, EventArgs e)
+        {
+            string procName = txtboxProcess.Text.Trim();
+            if (!string.IsNullOrEmpty(procName))
+            {
+                Process myProcess = new Process();
+                myProcess.StartInfo = new ProcessStartInfo(procName);
+                try
+                {
+                    myProcess.Start();
+                    ShowInfo($"Process is started: {procName}");
+                    RefreshList();
+                }
+                catch(Exception ex)
+                {
+                    ShowError($"Process not started: {procName}\nError message: {ex.Message}");
+                }
+            }
+        }
+
+        private void btnKillProcess_Click(object sender, EventArgs e)
+        {
+            if (listProcess.SelectedItem != null)
+            {
+                string selectedProcName = listProcess.SelectedItem.ToString();
+
+                Process[] processes = Process.GetProcessesByName(selectedProcName);
+
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                }
+
+                ShowInfo($"Killed process: {selectedProcName}");
+                RefreshList();
+            }
+            else
+            {
+                ShowError("Select process from list");
+            }
+        }
+    }
+}
